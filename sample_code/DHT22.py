a058b5327199f69d4b91e868f9d8874b0f0861c2
Simon Que <simon_justin_que@dlsu.edu.ph>
2022-12-22 16:37:55 +0800

    debug dht

--- sample_code/DHT22.py
+++ sample_code/DHT22.py
@@ -9,24 +9,40 @@ try:
 except:
     print("DHT library not present")
 
-dhts = []
-print(board)
-dhts.append(adafruit_dht.DHT22(board.D4))
-dhts.append(adafruit_dht.DHT22(board.D17))
-temperatures = [0]*2
-humidities = [0]*2
+dht = adafruit_dht.DHT22(board.D4)
+temperature = 0
+humidity = 0
 
 while True:
     try:
-        for i in range(len(dhts)):
-            temperatures[i] = dhts[i].temperature
-            humidities[i] = dhts[i].humidity
+        temperature = dht.temperature
+        humidity = dht.humidity
 
     except Exception as e:
         print('read error ')
         print(e)
 
-    for i in range(len(dhts)):
-        print("temp[{i}]={temperature}, humd[{i}]={humidity}".format(i, temperatures[i], i, humidities[i]))
-
+    print("temp={temperature}, humd={humidity}".format(temperature, humidity))
     time.sleep(3000)
+
+# dhts = []
+# print(board)
+# dhts.append(adafruit_dht.DHT22(board.D4))
+# dhts.append(adafruit_dht.DHT22(board.D17))
+# temperatures = [0]*2
+# humidities = [0]*2
+#
+# while True:
+#     try:
+#         for i in range(len(dhts)):
+#             temperatures[i] = dhts[i].temperature
+#             humidities[i] = dhts[i].humidity
+#
+#     except Exception as e:
+#         print('read error ')
+#         print(e)
+#
+#     for i in range(len(dhts)):
+#         print("temp[{i}]={temperature}, humd[{i}]={humidity}".format(i, temperatures[i], i, humidities[i]))
+#
+#     time.sleep(3000)
