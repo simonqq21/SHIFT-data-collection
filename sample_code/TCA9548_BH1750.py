13e74444bd96cf3c8a0436dea612881f94a792ea
Simon Que <simon_justin_que@dlsu.edu.ph>
2023-01-12 16:44:54 +0800

    a

--- sample_code/TCA9548_BH1750.py
+++ sample_code/TCA9548_BH1750.py
@@ -21,8 +21,8 @@ for si in range(bhcount):
             bhs.append(adafruit_bh1750.BH1750(tca[si%8], address=92))
         else:
             bhs.append(adafruit_bh1750.BH1750(tca[si%8]))
-        except:
-            print("not running on Pi or device not connected properly")
+    except:
+        print("not running on Pi or device not connected properly")
 
 luxs = [0]*bhcount
 while True:
