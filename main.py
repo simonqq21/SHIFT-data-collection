bb626bc1947a500fd24740628050bbc42346081c
Simon Que <simon_justin_que@dlsu.edu.ph>
2023-05-10 13:21:10 +0800

    removed GUI because it is blocking the code from starting

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from system import *
 from config import *
-from gui import * 
 
-# gui init
-gui = GUI()
 
 if __name__ == "__main__":
-    gui.mainloop()
+    # system init 
+    system = System() 
+    system.start() 
     
     
