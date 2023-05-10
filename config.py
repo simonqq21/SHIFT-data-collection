4ebfe3cd566b46e710258831b0d6be6a066919ff
Simon Que <simon_justin_que@dlsu.edu.ph>
2023-05-10 17:23:49 +0800

    added dirs and files for flask app

--- config.py
+++ config.py
@@ -1,67 +1,69 @@
 from datetime import datetime, date, time, timedelta
 import os 
-# MQTT broker 
-mqttIP = "ccscloud2.dlsu.edu.ph"
-mqttPort = 20010
-clientname = "DLSU_SHIFT"
-# MQTT data constants
-expt_num = 0 
-sitename = "DLSU-BLAST"
-# MQTT topics 
-main_topic = "sensor/dlsu/node-1/"
-suffix_temperature = "temperature"
-suffix_humidity = "humidity"
-suffix_lightintensity = "light_intensity"
-suffix_soilmoisture = "soil_moisture"
-suffix_pH = "solution_pH"
-suffix_EC = "solution_EC"
-suffix_camera = "camera"
 
-# csv filepath and filename 
-csv_filepath = "/home/pi/blast/csv/"
-csv_filename = "BLAST_sensors.csv"
-# image filepath and filename 
-images_filepath = "/home/pi/blast/images/"
-images_filename_format = "IMG_{}.jpg"
-# create image filepath if it doesn't exist 
-debug = 1
+class Config(object):
+    # MQTT broker 
+    mqttIP = "ccscloud2.dlsu.edu.ph"
+    mqttPort = 20010
+    clientname = "DLSU_SHIFT"
+    # MQTT data constants
+    expt_num = 0 
+    sitename = "DLSU-BLAST"
+    # MQTT topics 
+    main_topic = "sensor/dlsu/node-1/"
+    suffix_temperature = "temperature"
+    suffix_humidity = "humidity"
+    suffix_lightintensity = "light_intensity"
+    suffix_soilmoisture = "soil_moisture"
+    suffix_pH = "solution_pH"
+    suffix_EC = "solution_EC"
+    suffix_camera = "camera"
 
-# dataframe template for sensor data packet
-sensor_data = {
-    'datetime': [],
-    'expt_num': [],
-    'sitename': [],
-    'type': [],
-    'index': [],
-    'value': [],
-}
+    # csv filepath and filename 
+    csv_filepath = "/home/pi/blast/csv/"
+    csv_filename = "BLAST_sensors.csv"
+    # image filepath and filename 
+    images_filepath = "/home/pi/blast/images/"
+    images_filename_format = "IMG_{}.jpg"
+    # create image filepath if it doesn't exist 
+    debug = 1
 
-# dataframe template for image data packet
-image_data = {
-    'expt_num': [],
-    'sitename': [],
-    'type': [],
-    'index': [],
-    'filename': [],
-    'imagedata': [],
-}
-# camera_columns = ["type", "index", "filename", "binary_image"]
+    # dataframe template for sensor data packet
+    sensor_data = {
+        'datetime': [],
+        'expt_num': [],
+        'sitename': [],
+        'type': [],
+        'index': [],
+        'value': [],
+    }
 
-# time of the day when sensor logging will start and end 
-sensor_logging_start = time(hour=0, minute=0)
-sensor_logging_end = time(hour=23, minute=59)
+    # dataframe template for image data packet
+    image_data = {
+        'expt_num': [],
+        'sitename': [],
+        'type': [],
+        'index': [],
+        'filename': [],
+        'imagedata': [],
+    }
+    # camera_columns = ["type", "index", "filename", "binary_image"]
 
-# root dir of the program
-program_root = os.path.realpath(os.path.dirname(__file__)) 
+    # time of the day when sensor logging will start and end 
+    sensor_logging_start = time(hour=0, minute=0)
+    sensor_logging_end = time(hour=23, minute=59)
 
-# interval to check all sensors and actuators 
-checkingInterval = timedelta(seconds=10)
-# interval to poll sensors and upload sensor data
-sensorPollingInterval = timedelta(minutes=60) # 30 minutes
+    # root dir of the program
+    program_root = os.path.realpath(os.path.dirname(__file__)) 
 
-# GPIO pins 
-growLightPin = 18
-cameraLightPin = 27
-cameraButtonPin = 9
-pumpPins = (22, 23, 24) 
-pumpButtonPin = 10
+    # interval to check all sensors and actuators 
+    checkingInterval = timedelta(seconds=10)
+    # interval to poll sensors and upload sensor data
+    sensorPollingInterval = timedelta(minutes=60) # 30 minutes
+
+    # GPIO pins 
+    growLightPin = 18
+    cameraLightPin = 27
+    cameraButtonPin = 9
+    pumpPins = (22, 23, 24) 
+    pumpButtonPin = 10
--- system.py
+++ system.py
@@ -11,26 +11,31 @@ MQTT data packet format
 }
 '''
 try:
+    import json
     import os
-    from time import sleep 
-    import json 
-    from datetime import datetime, date, time, timedelta
-    from hardware.growlights_camera import LightsCamera
-    from hardware.irrigation_pumps import SyncedPumps
-    from hardware.onewire_temperature_humidity import DHT22
-    from hardware.i2c_lightintensity import BH1750, TCA9548A
-    from hardware.analog_soilmoisture_ph_ec import ADS1115, SoilMoistureSensor, PH4502C, TDSMeter
-    import pandas as pd 
+    import threading
+    from datetime import date, datetime, time, timedelta
+    from time import sleep
+
     import numpy as np
     import paho.mqtt.client as mqtt
-    import threading
+    import pandas as pd
+
+    from hardware.analog_soilmoisture_ph_ec import (ADS1115, PH4502C,
+                                                    SoilMoistureSensor,
+                                                    TDSMeter)
+    from hardware.growlights_camera import LightsCamera
+    from hardware.i2c_lightintensity import BH1750, TCA9548A
+    from hardware.irrigation_pumps import SyncedPumps
+    from hardware.onewire_temperature_humidity import DHT22
 except Exception as e:
     print(e)
 try:
-    from hardware.pi_interfaces import onewires, i2c
+    from hardware.pi_interfaces import i2c, onewires
 except:
     print("main.py not running on RPi")
-from config import *
+from config import Config
+
 
 class System(): 
     def __init__(self):
@@ -68,7 +73,7 @@ class System():
     def processSensorDataForPublishing(self, datetime, type, index, rawsensordata):
         global debug
         global expt_num, sitename
-        data = sensor_data
+        data = Config.sensor_data
         data["datetime"] = [datetime]
         data["expt_num"] = [expt_num]
         data["sitename"]= [sitename]
@@ -86,7 +91,7 @@ class System():
     '''
     def saveAndPublishData(self, df, sensorPublishTopic):
         print(df)
-        df.to_csv(csv_filepath + csv_filename, mode='a', index=False, header=False)
+        df.to_csv(Config.csv_filepath + Config.csv_filename, mode='a', index=False, header=False)
         try:
             self.client.publish(sensorPublishTopic, df.to_json())
         except:
@@ -98,7 +103,7 @@ class System():
     def processImageDataForPublishing(self, type, index, filename, binaryImage):
         global debug
         global expt_num, sitename
-        data = image_data
+        data = Config.image_data
         data["expt_num"] = [expt_num]
         data["sitename"]= [sitename]
         data["type"]= [type]
@@ -121,33 +126,33 @@ class System():
 
     def filesInit(self):
         # create directories for collected data 
-        os.makedirs(csv_filepath, exist_ok=True)
-        os.makedirs(images_filepath, exist_ok=True)
+        os.makedirs(Config.csv_filepath, exist_ok=True)
+        os.makedirs(Config.images_filepath, exist_ok=True)
 
         # create csv file for sensor data if it doesnt exist 
-        df = pd.DataFrame.from_dict(sensor_data, orient='columns')
+        df = pd.DataFrame.from_dict(Config.sensor_data, orient='columns')
         mode = 'w'
         index=False
         header=True
-        if os.path.exists(csv_filepath + csv_filename):
+        if os.path.exists(Config.csv_filepath + Config.csv_filename):
             mode = 'a'
             header=False
             print('exists!')
-        df.to_csv(csv_filepath + csv_filename, mode=mode, index=index, header=header) 
+        df.to_csv(Config.csv_filepath + Config.csv_filename, mode=mode, index=index, header=header) 
         self.columns = df.columns.values
         print(self.columns)
         # columns for image dataframe
-        self.camera_columns = np.array(list(image_data.keys()))
+        self.camera_columns = np.array(list(Config.image_data.keys()))
         print(self.camera_columns)
 
     def MQTTInit(self):
         # mqtt client init
         try:
-            self.client = mqtt.Client(clientname)
+            self.client = mqtt.Client(Config.clientname)
             self.client.on_connect = self.on_connect
             self.client.on_message = self.on_message
             self.client.on_publish = self.on_publish
-            self.client.connect(mqttIP, mqttPort)
+            self.client.connect(Config.mqttIP, Config.mqttPort)
             print(self.client)
             self.client.loop_start()
         except Exception as e:
@@ -191,9 +196,9 @@ class System():
         self.adss[2].addTDSMeter()
 
         # initialize grow lights and camera with camera light
-        self.lightscamera = LightsCamera(growLightPin, cameraLightPin, cameraButtonPin, program_root+"/growlight_interval.json", program_root+"/camera_interval.json", images_filepath, images_filename_format)
+        self.lightscamera = LightsCamera(Config.growLightPin, Config.cameraLightPin, Config.cameraButtonPin, Config.program_root+"/growlight_interval.json", Config.program_root+"/camera_interval.json", Config.images_filepath, Config.images_filename_format)
         # initialize irrigation pumps
-        self.pumps = SyncedPumps(pumpPins, pumpButtonPin, program_root+"/pumps_interval.json")
+        self.pumps = SyncedPumps(Config.pumpPins, Config.pumpButtonPin, Config.program_root+"/pumps_interval.json")
 
     def activatePump(self, index): 
         thread = threading.Thread(target=self.pumps.pumps[index].pumpOn, daemon=True)
@@ -209,17 +214,17 @@ class System():
             print(dht.getHumidity())
             curr_temperature = dht.getTemperature()
             curr_humidity = dht.getHumidity()
-            df_temperature = self.processSensorDataForPublishing(sensorTimeStamp, suffix_temperature, index, curr_temperature)
-            self.saveAndPublishData(df_temperature, main_topic+suffix_temperature)
-            df_humidity = self.processSensorDataForPublishing(sensorTimeStamp, suffix_humidity, index, curr_humidity)
-            self.saveAndPublishData(df_humidity, main_topic+suffix_humidity)
+            df_temperature = self.processSensorDataForPublishing(sensorTimeStamp, Config.suffix_temperature, index, curr_temperature)
+            self.saveAndPublishData(df_temperature, Config.main_topic+Config.suffix_temperature)
+            df_humidity = self.processSensorDataForPublishing(sensorTimeStamp, Config.suffix_humidity, index, curr_humidity)
+            self.saveAndPublishData(df_humidity, Config.main_topic+Config.suffix_humidity)
             index += 1
         # light intensity from BH1750 
         curr_lightIntensities = self.tca.getLightIntensities()
         index = 0
         for li in curr_lightIntensities:
-            df_lightintensity = self.processSensorDataForPublishing(sensorTimeStamp, suffix_lightintensity, index, li)
-            self.saveAndPublishData(df_lightintensity, main_topic+suffix_lightintensity)
+            df_lightintensity = self.processSensorDataForPublishing(sensorTimeStamp, Config.suffix_lightintensity, index, li)
+            self.saveAndPublishData(df_lightintensity, Config.main_topic+Config.suffix_lightintensity)
             index += 1
         print("22")
         # soil moisture, pH, and EC from soil moisture sensors. PH-4502C, and TDS Meter 1.0 
@@ -235,18 +240,18 @@ class System():
                 curr_solutionECs.append(ec)
         index = 0 
         for sm in curr_soilmoistures:
-            df_soilmoisture = self.processSensorDataForPublishing(sensorTimeStamp, suffix_soilmoisture, index, sm)
-            self.saveAndPublishData(df_soilmoisture, main_topic+suffix_soilmoisture)
+            df_soilmoisture = self.processSensorDataForPublishing(sensorTimeStamp, Config.suffix_soilmoisture, index, sm)
+            self.saveAndPublishData(df_soilmoisture, Config.main_topic+Config.suffix_soilmoisture)
             index += 1
         index = 0 
         for pH in curr_solutionpHs:
-            df_solutionpH = self.processSensorDataForPublishing(sensorTimeStamp, suffix_pH, index, pH)
-            self.saveAndPublishData(df_solutionpH, main_topic+suffix_pH)
+            df_solutionpH = self.processSensorDataForPublishing(sensorTimeStamp, Config.suffix_pH, index, pH)
+            self.saveAndPublishData(df_solutionpH, Config.main_topic+Config.suffix_pH)
             index += 1
         index = 0 
         for ec in curr_solutionECs:
-            df_solutionEC = self.processSensorDataForPublishing(sensorTimeStamp, suffix_EC, index, ec)
-            self.saveAndPublishData(df_solutionEC, main_topic+suffix_EC)
+            df_solutionEC = self.processSensorDataForPublishing(sensorTimeStamp, Config.suffix_EC, index, ec)
+            self.saveAndPublishData(df_solutionEC, Config.main_topic+Config.suffix_EC)
             index += 1
 
     def captureImage(self):
@@ -259,8 +264,8 @@ class System():
             print("binary image received")
             index=0
             cameraTimeStamp = datetime.now().strftime("%m/%d/%Y %H:%M")
-            df_image = self.processImageDataForPublishing(suffix_camera, index, self.lightscamera.filename, self.lightscamera.binaryImage)
-            self.publishImage(df_image, main_topic+suffix_camera)
+            df_image = self.processImageDataForPublishing(Config.suffix_camera, index, self.lightscamera.filename, self.lightscamera.binaryImage)
+            self.publishImage(df_image, Config.main_topic+Config.suffix_camera)
             self.lightscamera.newImage = 0 
 
     def setCameraLightOperation(self, mode):
@@ -279,7 +284,7 @@ class System():
             self.lightscamera.getCameraIntervalsPerDay()
             
         # loop to check the camera, growlights, and pump
-        if (datetime.now() - self.intervalLastChecked >= checkingInterval):
+        if (datetime.now() - self.intervalLastChecked >= Config.checkingInterval):
             self.intervalLastChecked = datetime.now()
             # loop to check switch grow lights
             self.lightscamera.pollGrowLights(self.datetimenow)
@@ -294,8 +299,8 @@ class System():
             # loop to check and run irrigation pumps
             self.pumps.pollPumps(self.datetimenow)
         # loop to gather sensor data from all sensors, package it into json, and send it via MQTT 
-        if (self.datetimenow - self.sensorsLastPolled >= sensorPollingInterval and \
-            self.datetimenow >= datetime.combine(self.datetimenow.date(), sensor_logging_start) and \
-            self.datetimenow <= datetime.combine(self.datetimenow.date(), sensor_logging_end)):
+        if (self.datetimenow - self.sensorsLastPolled >= Config.sensorPollingInterval and \
+            self.datetimenow >= datetime.combine(self.datetimenow.date(), Config.sensor_logging_start) and \
+            self.datetimenow <= datetime.combine(self.datetimenow.date(), Config.sensor_logging_end)):
             self.sensorsLastPolled = datetime.now()
             self.captureSensors()
