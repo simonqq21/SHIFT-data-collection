45f5a31beb7c73d8b9ca7130ad923eb801a1f15d
simonqq21 <72963700+simonqq21@users.noreply.github.com>
2023-06-01 02:52:08 +0000

    defined code for sensors

--- mainserver.py
+++ mainserver.py
@@ -88,32 +88,8 @@ class SyncServer():
         if status:
             server.send(command.encode('utf-8')) 
             if Config.debug:
-                print("sent sensors command") 
+                print("sent sensors command")    
 
-    
-
-    # compute for the grow light intervals for each new day
-    def getGrowLightIntervalsPerDay(self):
-        self.growLightDailyIntervals = [] 
-        for interval in self.growLightIntervals: 
-            newGrowLightInterval = {} 
-            newGrowLightInterval["on_time"] = datetime.combine(date.today(), interval["on_time"])
-            newGrowLightInterval["duration"] = interval["duration"]
-            newGrowLightInterval["off_time"] = newGrowLightInterval["on_time"] + interval["duration"]
-            self.growLightDailyIntervals.append(newGrowLightInterval)
-        print(self.growLightDailyIntervals)
-
-    # compute for the image capturing intervals for each new day
-    def getCameraIntervalsPerDay(self):
-        self.cameraDailyIntervals = []
-        for interval in self.cameraIntervals: 
-            newCameraInterval = {}
-            newCameraInterval["start_time"] = datetime.combine(date.today(), interval["start_time"]) 
-            newCameraInterval["interval"] = interval["interval"]
-            newCameraInterval["end_time"] = datetime.combine(date.today(), interval["end_time"]) 
-            self.cameraDailyIntervals.append(newCameraInterval)
-        print(self.cameraDailyIntervals)
-        
     def loop(self):
         # for timekeeping
         # self.datetimenow = datetime.combine(date.today(), time(hour=7, minute=0, second=0))
@@ -122,37 +98,40 @@ class SyncServer():
             self.datetimenow = datetime.now()
             timeNow = self.datetimenow.time
             # tell the sensors module to capture and transmit sensor data
-            # if the current time is in between the start and end times for sensor logging and 
-            if (self.datetimenow - self.sensorsLastPolled >= Config.sensorPollingInterval and \
+            '''
+            if the current time is in between the start and end times for sensor logging and if the 
+            time since last sensor logging has exceeded the set sensor logging interval  
+            '''
+            if (self.datetimenow - self.timeLastSensorsLogged >= Config.sensorLoggingInterval and \
                 self.datetimenow >= datetime.combine(self.datetimenow.date(), Config.sensor_logging_start) and \
                 self.datetimenow <= datetime.combine(self.datetimenow.date(), Config.sensor_logging_end)): 
-                # if the time since last sensor logging has exceeded the set sensor logging interval 
-                if (self.datetimenow - lastTimeSensorsLogged > Config.sensorLoggingInterval): 
-                    lastTimeSensorsLogged = self.datetimenow
-            # tell the camera module to capture and transmit image data
+                self.timeLastSensorsLogged = self.datetimenow
+                self.sensorsLog()
 
+            # tell the camera module to capture and transmit image data
+            
             # tell the pumps module to turn the pumps on for a certain duration
 
             # tell the lights module to turn on the lights to the correct mode
   
 
               
-            # loop to check the camera, growlights, and pump
-            if (datetime.now() - self.intervalLastChecked >= Config.checkingInterval):
-                self.intervalLastChecked = datetime.now()
-                # loop to check switch grow lights
-                self.lightscamera.pollGrowLights(self.datetimenow)
-                print("val={}".format(self.lightscamera.growlightval))
-                '''
-                while the camera is capturing an image, the growlight code must be overriden.
-                '''
-                # loop to capture image 
-                self.lightscamera.pollCamera(self.datetimenow)
-                self.publishNewImage()
+            # # loop to check the camera, growlights, and pump
+            # if (datetime.now() - self.intervalLastChecked >= Config.checkingInterval):
+            #     self.intervalLastChecked = datetime.now()
+            #     # loop to check switch grow lights
+            #     self.lightscamera.pollGrowLights(self.datetimenow)
+            #     print("val={}".format(self.lightscamera.growlightval))
+            #     '''
+            #     while the camera is capturing an image, the growlight code must be overriden.
+            #     '''
+            #     # loop to capture image 
+            #     self.lightscamera.pollCamera(self.datetimenow)
+            #     self.publishNewImage()
 
-                # loop to check and run irrigation pumps
-                self.pumps.pollPumps(self.datetimenow)
-            # loop to gather sensor data from all sensors, package it into json, and send it via MQTT 
-                self.sensorsLastPolled = datetime.now()
-                self.captureSensors()
-            sleep(1)
+            #     # loop to check and run irrigation pumps
+            #     self.pumps.pollPumps(self.datetimenow)
+            # # loop to gather sensor data from all sensors, package it into json, and send it via MQTT 
+            #     self.sensorsLastPolled = datetime.now()
+            #     self.captureSensors()
+            # sleep(1)
