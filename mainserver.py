30127d505b2558f15c901fb323076ba18fac6cbd
simonqq21 <72963700+simonqq21@users.noreply.github.com>
2023-06-02 09:05:57 +0000

    moved the current logic into the loop thread functions

--- mainserver.py
+++ mainserver.py
@@ -21,8 +21,7 @@ class SyncServer():
         '''
         Camera and sensors are periodic with a start and end time, while 
         pumps and lights are periodic with defined opening and closing times.
-        '''
-                                                 
+        '''                                         
         # used for timing periodic events such as the cameras and sensors
         self.timeLastSensorsLogged = datetime(year=1970, month=1, day=1)
         self.timeLastCameraCaptured = datetime(year=1970, month=1, day=1)
@@ -36,6 +35,9 @@ class SyncServer():
         do not capture readings when the white camera light is on 
         '''
         self.whiteLightOn = False
+
+        self.datetimenow = datetime.now()
+
 
     def connect(self, server, HOST, PORT, retries=8, timeout_per_retry=5):
         for i in range(retries):
@@ -59,8 +61,39 @@ class SyncServer():
             if Config.debug:
                 print("sent pumps command")
 
-    def pumpsThreadLoop(self):
-        pass 
+    def pumpsThreadLoop(self):        
+        pumpsDateTime = datetime.now()
+        timeOffset = datetime.now() - pumpsDateTime # offset timedelta 
+        while True:
+            timeNow = pumpsDateTime.time()
+            dateNow = pumpsDateTime.date()
+            # code to run at the start of each day
+            if (timeNow >= time(hour=0, minute=0, second=0) and \
+                    timeNow <= time(hour=0, minute=0, second=59)):
+                if Config.debug:
+                    print("new day")
+                    # reset pumps on count each day
+                    for i in range(len(self.pumpsOnCount)):
+                        self.pumpsOnCount[i] = 0
+            pumpsDateTime = datetime.now() - timeOffset
+
+                        # tell the pumps module to turn the pumps on for a certain duration
+            ''' 
+            iterate through each pump and run it with start time and duration
+            '''
+            for pumpIndex in range(len(Config.pumps_start_duration)):
+                for (start, duration) in Config.pumps_start_duration[pumpIndex]:
+                    # get the number of times the pump is activated in a day
+                    maxOnCount = len(Config.pumps_start_duration[pumpIndex])
+                    if (self.datetimenow >= datetime.combine(self.datetimenow.date(), start) and \
+                        self.datetimenow <= datetime.combine(self.datetimenow.date(), \
+                                                             start + timedelta(seconds=59)) and \
+                            self.pumpsOnCount[pumpIndex] < maxOnCount):
+                        pumpOnThread = threading.Thread(target=self.pumpsControl, args=(pumpIndex, duration))
+                        pumpOnThread.start()
+                        if Config.debug:
+                            print(f"sent pump {pumpIndex} start command")
+                        self.pumpsOnCount[pumpIndex] += 1
 
     '''
     lightType is either 'p', 'w', or 'flash'
@@ -85,7 +118,30 @@ class SyncServer():
             # print(server.recv(1024))
 
     def lightsThreadLoop(self):
-        pass 
+        lightsDateTime = self.datetimenow
+        while True:
+            timeNow = self.datetimenow.time()
+            dateNow = self.datetimenow.date()
+            # code to run at the start of each day
+            if (self.datetimenow.time() >= time(hour=0, minute=0, second=0) and \
+                    self.datetimenow.time() <= time(hour=0, minute=0, second=59)):
+                if Config.debug:
+                    print("new day")
+                # insert code to run at the start of each day
+            
+            self.datetimenow += 1
+        
+                        # tell the lights module to turn on the grow lights to the correct mode
+            for (start, duration) in Config.growlights_on_times_durations:
+                if (self.datetimenow >= datetime.combine(self.datetimenow.date(), start) and \
+                        self.datetimenow <= datetime.combine(self.datetimenow.date(), \
+                                                             start + duration)):
+                    duration = datetime.combine(self.datetimenow.date(), start) + duration - self.datetimenow
+                    growLightsOnThread = threading.Thread(target=self.lightsControl, args=('p', duration))
+                    growLightsOnThread.start() 
+                    if Config.debug:
+                        print("sent growlights on command") 
+
 
     def cameraCapture(self):
         PORT = 12004
@@ -102,7 +158,33 @@ class SyncServer():
         self.whiteLightOn = False
 
     def cameraThreadLoop(self):
-        pass 
+        cameraDateTime = self.datetimenow
+        while True:
+            timeNow = cameraDateTime.time()
+            dateNow = cameraDateTime.date()
+            # code to run at the start of each day
+            if (self.datetimenow.time() >= time(hour=0, minute=0, second=0) and \
+                    self.datetimenow.time() <= time(hour=0, minute=0, second=59)):
+                if Config.debug:
+                    print("new day")
+                # insert code to run at the start of each day
+
+                        # tell the camera module to capture and transmit image data
+            '''
+            if the current time is in between the start and end times for camera capture and if the 
+            time since last camera capture has exceeded the set camera capture interval  
+            '''
+            if (self.datetimenow - self.timeLastCameraCaptured >= Config.cameraCaptureInterval and \
+                self.datetimenow >= datetime.combine(self.datetimenow.date(), Config.camera_capture_start) and \
+                    self.datetimenow <= datetime.combine(self.datetimenow.date(), Config.camera_capture_end)):
+                self.timeLastCameraCaptured = self.datetimenow
+                # flash the white light and capture an image
+                cameraLightThread = threading.Thread(target=self.lightsControl, args=("flash,"))
+                cameraLightThread.start()
+                # capture the image
+                cameraCaptureThread = threading.Thread(target=self.cameraCapture)
+                cameraCaptureThread.start()
+
 
     def sensorsLog(self):
         PORT = 12005
@@ -120,15 +202,9 @@ class SyncServer():
             if Config.debug:
                 print("sent sensors command")
 
-    def sensorThreadLoop(self):
-        pass
-
-    def loop(self):
-        # for timekeeping
-        # self.datetimenow = datetime.combine(date.today(), time(hour=7, minute=0, second=0))
-
+    def sensorsThreadLoop(self):
         while True:
-            self.datetimenow = datetime.now()
+            
             timeNow = self.datetimenow.time()
             dateNow = self.datetimenow.date()
             # code to run at the start of each day
@@ -136,9 +212,7 @@ class SyncServer():
                     self.datetimenow.time() <= time(hour=0, minute=0, second=59)):
                 if Config.debug:
                     print("new day")
-                    # reset pumps on count each day
-                    for i in range(len(self.pumpsOnCount)):
-                        self.pumpsOnCount[i] = 0
+                # insert code to run at the start of each day
 
             # tell the sensors module to capture and transmit sensor data
             '''
@@ -153,49 +227,14 @@ class SyncServer():
                 sensorThread = threading.Thread(target=self.sensorsLog)
                 sensorThread.start()
 
-            # tell the camera module to capture and transmit image data
-            '''
-            if the current time is in between the start and end times for camera capture and if the 
-            time since last camera capture has exceeded the set camera capture interval  
-            '''
-            if (self.datetimenow - self.timeLastCameraCaptured >= Config.cameraCaptureInterval and \
-                self.datetimenow >= datetime.combine(self.datetimenow.date(), Config.camera_capture_start) and \
-                    self.datetimenow <= datetime.combine(self.datetimenow.date(), Config.camera_capture_end)):
-                self.timeLastCameraCaptured = self.datetimenow
-                # flash the white light and capture an image
-                cameraLightThread = threading.Thread(target=self.lightsControl, args=("flash,"))
-                cameraLightThread.start()
-                # capture the image
-                cameraCaptureThread = threading.Thread(target=self.cameraCapture)
-                cameraCaptureThread.start()
 
-            # tell the pumps module to turn the pumps on for a certain duration
-            ''' 
-            iterate through each pump and run it with start time and duration
-            '''
-            for pumpIndex in range(len(Config.pumps_start_duration)):
-                for (start, duration) in Config.pumps_start_duration[pumpIndex]:
-                    # get the number of times the pump is activated in a day
-                    maxOnCount = len(Config.pumps_start_duration[pumpIndex])
-                    if (self.datetimenow >= datetime.combine(self.datetimenow.date(), start) and \
-                        self.datetimenow <= datetime.combine(self.datetimenow.date(), \
-                                                             start + timedelta(seconds=59)) and \
-                            self.pumpsOnCount[pumpIndex] < maxOnCount):
-                        pumpOnThread = threading.Thread(target=self.pumpsControl, args=(pumpIndex, duration))
-                        pumpOnThread.start()
-                        if Config.debug:
-                            print(f"sent pump {pumpIndex} start command")
-                        self.pumpsOnCount[pumpIndex] += 1
-
-            # tell the lights module to turn on the grow lights to the correct mode
-            for (start, duration) in Config.growlights_on_times_durations:
-                if (self.datetimenow >= datetime.combine(self.datetimenow.date(), start) and \
-                        self.datetimenow <= datetime.combine(self.datetimenow.date(), \
-                                                             start + duration)):
-                    duration = datetime.combine(self.datetimenow.date(), start) + duration - self.datetimenow
-                    growLightsOnThread = threading.Thread(target=self.lightsControl, args=('p', duration))
-                    growLightsOnThread.start() 
-                    if Config.debug:
-                        print("sent growlights on command") 
-
-          # sleep(1)
+    def loop(self):
+        # self.datetimenow = datetime.combine(date.today(), time(hour=7, minute=0, second=0))
+        pumpsThread = threading.Thread(target=self.pumpsThreadLoop) 
+        pumpsThread.start() 
+        lightsThread = threading.Thread(target=self.lightsThreadLoop) 
+        lightsThread.start() 
+        sensorsThread = threading.Thread(target=self.sensorsThreadLoop)
+        sensorsThread.start()
+        cameraThreadLoop = threading.Thread(target=self.cameraThreadLoop)
+        cameraThreadLoop.start()
