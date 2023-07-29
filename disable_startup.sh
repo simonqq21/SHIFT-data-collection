af54c1c7ce16322207ac8d48292968286082e74b
simonqq21 <72963700+simonqq21@users.noreply.github.com>
2023-07-29 10:17:17 +0800

    created disable_startup and enable_startup scripts

---
+++ disable_startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo systemctl disable blast_camera.service
+sudo systemctl disable blast_lights.service
+sudo systemctl disable blast_mainserver.service
+sudo systemctl disable blast_pumps.service
+sudo systemctl disable blast_sensors.service
---
+++ enable_startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo systemctl enable blast_camera.service
+sudo systemctl enable blast_lights.service
+sudo systemctl enable blast_mainserver.service
+sudo systemctl enable blast_pumps.service
+sudo systemctl enable blast_sensors.service
